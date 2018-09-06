a5ad59a273cbc0ad720ac8d31653bd0644a33a7c
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-09-06 15:22:39 +0200

    first version

---
+++ download.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#First parameter: git username
+#Second parameter: -t -> do not use tags
+
+trap exit ERR
+
+if [ "$2" != "-t" ] ; then
+	echo "USE TAGS"
+	TAG_PACKETLIB=v3.2.0
+	TAG_QLBASE=
+	TAG_TELEM=
+	TAG_SCI2=
+	TAG_GENERATOR=
+fi
+
+echo "--------------"
+git clone https://$1@https://github.com/ASTRO-BO/PacketLib.git
+cd PacketLib
+if [ "$2" != "-t" ]; then
+	git checkout $TAG_PACKETLIB
+else
+	git checkout $TAG_PACKETLIB
+fi
+cd ..
+
+echo "--------------"
+git clone https://$1@github.com/ASTRO-BO/libQLBase.git
+cd libQLBase
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_QLBASE
+else
+        echo "Use master version"
+fi
+cd ..
+
+echo "--------------"
+git clone https://$1@github.com/AGILESCIENCE/libagiletelem.git 
+cd libagiletelem
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_TELEM
+else
+        echo "Use master version"
+fi
+cd ..
+
+echo "--------------"
+git clone https://$1@github.com/AGILESCIENCE/agilesci2.git    
+cd agilesci2
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_SCI2
+else
+        echo "Use master version"
+fi
+cd ..
+
+
+echo "--------------"
+git clone https://$1@github.com/AGILESCIENCE/gtImporterELtelem.git
+cd gtImporterELtelem
+if [ "$2" != "-t" ]; then
+        git checkout $TAG_GENERATOR
+else
+        echo "Use master version"
+fi
+cd ..
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+trap exit ERR
+
+if [ "$1" == "-j" ] ; then
+    parallel=$1
+fi
+
+### check environment variables
+if [ -z "$AGILE" ] || [ -z $(env | grep "AGILE=") ] ; then
+    echo "AGILE environment variable not set. Abort."
+    exit
+fi
+if [ -z "$CFITSIO" ] || [ -z $(env | grep "CFITSIO=") ] ; then
+    echo "CFITSIO environment variable not set. Abort."
+    exit
+fi
+if [ -z "$ROOTSYS" ] || [ -z $(env | grep "ROOTSYS=") ] ; then
+    echo "ROOTSYS environment variable not set. Abort."
+    exit
+fi
+
+if [ "$1" == "clean" ] ; then
+    cd PacketLib
+    make clean
+    cd libQLBase
+    make clean
+    cd libagiletelem
+    make clean
+    exit
+fi
+
+echo "install PacketLib"
+cd PacketLib
+make install prefix=$AGILE
+cd ..
+
+echo "install libQLBase"
+cd libQLBase
+cmake CMakeLists.txt -DCMAKE_INSTALL_PREFIX=$AGILE
+cd ..
+
+echo "install agiletelem"
+cd libagiletelem
+make ${parallel} install prefix=$AGILE
+cd ..
+
+echo "install agilesci2"
+cd agilesci2
+make ${parallel} install prefix=$AGILE
+cd ..
+
+echo "install gtImporterELtelem"
+cd gtImporterELtelem
+make ${parallel} install prefix=$AGILE
+cd ..
+
+#if [ -z "$OPENCV" ] || [ -z $(env | grep "OPENCV=") ] ; then
+#    	echo "OPENCV environment variable not set. AG_extspot not installed."
+#else
+#	echo "install AG_extspot"
+#	cd agextspot-v2
+#	make ${parallel} install prefix=$AGILE
+#	cd ..
+#fi
